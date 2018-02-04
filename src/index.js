9411119d25b8962452d16da643d480864661977f
tarvainen <atte.tarvainen@protacon.com>
2018-02-04 18:31:20 +0200

    Indent

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const io = require('socket.io')();
 
 io.on('connection', (socket) => {
-    socket.on('notification', (data) => {
-        socket.broadcast.emit('notification', {
-            data: data
-        });
+  socket.on('notification', (data) => {
+    socket.broadcast.emit('notification', {
+        data: data
     });
+  });
 });
 
 io.listen(3000);
