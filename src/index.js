d32413825840ade860f9ab726f03adaf0e2420cb
tarvainen <atte.tarvainen@protacon.com>
2018-02-04 18:47:27 +0200

    Add simple REST support to push messages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-const io = require('socket.io')();
+const app    = require('express')();
+const parser = require('body-parser');
+const server = require('http').Server(app);
+const io     = require('socket.io')(server);
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.post('/notification', (req, res) => {
+  const content = {
+    title:   req.body.title,
+    content: req.body.content
+  };
+
+  io.sockets.emit('notification', content);
+
+  res.json(content);
+});
 
 io.on('connection', (socket) => {
   socket.on('notification', (data) => {
@@ -8,4 +25,4 @@ io.on('connection', (socket) => {
   });
 });
 
-io.listen(3000);
+server.listen(3000);
