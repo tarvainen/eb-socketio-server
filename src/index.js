de9ee1209bf829938d9fe3007e3aaf0451d716fd
tarvainen <atte.tarvainen@protacon.com>
2018-02-04 18:50:42 +0200

    CORS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const app    = require('express')();
 const parser = require('body-parser');
 const server = require('http').Server(app);
 const io     = require('socket.io')(server);
+const cors   = require('cors');
 
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.post('/notification', (req, res) => {
   const content = {
